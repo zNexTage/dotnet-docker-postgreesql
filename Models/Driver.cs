ff7fc3d64c7efa5cec39348939ffa915598782b3
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-07-09 13:27:34 -0300

    Criado model Driver.

---
+++ Models/Driver.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DockerDotnetMySql.Models;
+public class Driver
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    public int DriverNumber { get; set; }
+}
